0a9c5c801dccd0a76fad6aa5c155bd90ccf0625e
ckwong <ckwong@esrisingapore.com.sg>
2024-04-20 12:16:25 +0800

    Animated BigCard

--- lib/main.dart
+++ lib/main.dart
@@ -241,10 +241,16 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
+        child: AnimatedSize(
+          duration: Duration(milliseconds: 200),
+          child: Wrap(
+            children: [
+              Text(pair.first,
+                  style: style.copyWith(fontWeight: FontWeight.w200)),
+              Text(pair.second,
+                  style: style.copyWith(fontWeight: FontWeight.bold)),
+            ],
+          ),
         ),
       ),
     );
