c2da2eec81a053792017fe4a716851fc5c09f536
ckwong <ckwong@esrisingapore.com.sg>
2024-04-20 12:49:32 +0800

    Added shader to HistoryListView

--- lib/main.dart
+++ lib/main.dart
@@ -147,34 +147,47 @@ class HistoryListView extends StatefulWidget {
 class _HistoryListViewState extends State<HistoryListView> {
   final _key = GlobalKey();
 
+  static const Gradient _maskingGradient = LinearGradient(
+    // This gradient goes from fully transparent to fully opaque black...
+    colors: [Colors.transparent, Colors.black],
+    // ... from the top (transparent) to half (0.5) of the way to the bottom.
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
   @override
   build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     appState.historyListKey = _key;
 
-    return AnimatedList(
-      key: _key,
-      reverse: true,
-      padding: EdgeInsets.only(top: 100),
-      initialItemCount: appState.history.length,
-      itemBuilder: (context, index, animation) {
-        final pair = appState.history[index];
-        return SizeTransition(
-            sizeFactor: animation,
-            child: Center(
-                child: TextButton.icon(
-              onPressed: () {
-                appState.toggleFavourite();
-              },
-              label: Text(
-                pair.asLowerCase,
-                semanticsLabel: pair.asPascalCase,
-              ),
-              icon: appState.favourites.contains(pair)
-                  ? Icon(Icons.favorite, size: 12)
-                  : SizedBox(),
-            )));
-      },
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskingGradient.createShader(bounds),
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: _key,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: appState.history.length,
+        itemBuilder: (context, index, animation) {
+          final pair = appState.history[index];
+          return SizeTransition(
+              sizeFactor: animation,
+              child: Center(
+                  child: TextButton.icon(
+                onPressed: () {
+                  appState.toggleFavourite();
+                },
+                label: Text(
+                  pair.asLowerCase,
+                  semanticsLabel: pair.asPascalCase,
+                ),
+                icon: appState.favourites.contains(pair)
+                    ? Icon(Icons.favorite, size: 12)
+                    : SizedBox(),
+              )));
+        },
+      ),
     );
   }
 }
